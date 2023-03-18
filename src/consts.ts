298e6894d04327add9e5cf7248a68a7fd8c77041
sogud <github.sogud@outlook.com>
2023-03-18 13:57:18 +0800

    wip

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My blog.';
-export const SITE_DESCRIPTION = 'Welcome to my blog!';
+export const SITE_TITLE = 'hi there';
+export const SITE_DESCRIPTION = 'I\'m a web frontend engineer';
